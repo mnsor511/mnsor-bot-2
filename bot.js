57fbe1a69ce2e2c4f1f9c367fbd2884495d9b260
mnsor511 <42652535+mnsor511@users.noreply.github.com>
2018-09-03 13:53:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -896,7 +896,6 @@ const adminprefix = 'm!';
 
 
 
-const Discord = require("discord.js");
 const fs = require("fs");
 const lol = JSON.parse(fs.readFileSync("lol.json", "utf8"))
 const client = new Discord.Client();
