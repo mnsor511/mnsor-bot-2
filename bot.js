42b1f3cb8452ba4783235b4291258714c46d7253
mnsor511 <42652535+mnsor511@users.noreply.github.com>
2018-09-05 13:39:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -882,3 +882,70 @@ const adminprefix = 'm!';
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  client.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content === 'ادخل') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { 
+          message.reply('لقد دخلت الروم بنجاح !');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('يجب ان تكون في روم صوتي');
+    }
+  }
+});
+  
